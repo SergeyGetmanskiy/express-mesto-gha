0ea694b83d89f66d914808d947c836e11ba0845a
Sergey Getmanskiy <getmansky1997@yandex.ru>
2023-09-02 13:12:59 +0300

    PR14 ready for review

--- app.js
+++ app.js
@@ -38,6 +38,9 @@ app.post(
   '/signup',
   celebrate({
     body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().uri(),
       email: Joi.string().required().email(),
       password: Joi.string().required().min(8),
     }),
